36a20d2c938e7e3651504f3f2cc05f74a744234c
Tanja <tanja120a@gmail.com>
2021-08-29 17:02:04 +0200

    Create CSS loader and handle error durimg fetch data

---
+++ src/Components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Loader(){
+    return(
+        <div className="flex justify-center">
+            <div className="loader">
+            </div>
+            
+        </div>
+    )
+}
+
+export default Loader;
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -2,32 +2,63 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
 
+import Loader from '../Components/Loader';
+
 function Product(){
-    const[product, setProduct] = useState(null);
+    const[product, setProduct] = useState({
+        loading: false,
+        data: null,
+        error: false,
+    });
     const { id } = useParams();
     const url = `https://612b855922bb490017893b72.mockapi.io/products/${id}`;
-    let content = "Here come some products";
+    let content = null;
 
     useEffect(() => {
+        setProduct({
+            loading: true,
+            data: null,
+            error: false,
+        })
         axios.get(url).then((response) =>{
-            setProduct(response.data)
+            setProduct({
+                loading: false,
+                data: response.data,
+                error: false,
+            })
+        }).catch(() =>{
+            setProduct({
+                loading: false,
+                data: null,
+                error: true,
+            })
         })
     },[url])
 
-    if(product){
+    if(product.error){
+        content = <div>
+            There is an error, please refresh or try later...
+        </div>
+    }
+
+    if(product.loading){
+        content = <Loader/>
+    }
+
+    if(product.data){
         content =
             <div>
                 <h1 className="font-bold text-2xl mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
-                    <img src={product.images} alt={product.name}/>
+                    <img src={product.data.images} alt={product.data.name}/>
                 </div>
                 <div className="font-bold text-xl mb-3">
-                    $ {product.price}
+                    $ {product.data.price}
                 </div>
                 <div>
-                    {product.description}
+                    {product.data.description}
                 </div>
             </div>
         
